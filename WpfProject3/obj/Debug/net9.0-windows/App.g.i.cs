6b3c3e910dc0ad4d255af39d4d147d11d9a22e2f
MarkKuppens <mark.kuppens@student.odisee.be>
2025-03-30 15:18:09 +0200

    Update_layout
    Haley Icons pack etc

--- WpfProject3/obj/Debug/net9.0-windows/App.g.i.cs
+++ WpfProject3/obj/Debug/net9.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace WpfProject3 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/WpfProject3;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfProject3;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
